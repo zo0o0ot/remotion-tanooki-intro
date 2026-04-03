8a9bbadf811cd75532da39a4798c085c27296fd9
Ross Larson <zo0o0ot@gmail.com>
2026-04-03 01:05:38 -0500

    Adds a few changes

--- remotion.config.ts
+++ remotion.config.ts
@@ -6,8 +6,6 @@
  */
 
 import { Config } from "@remotion/cli/config";
-import { enableTailwind } from '@remotion/tailwind-v4';
 
 Config.setVideoImageFormat("jpeg");
 Config.setOverwriteOutput(true);
-Config.overrideWebpackConfig(enableTailwind);
--- src/Root.tsx
+++ src/Root.tsx
@@ -6,12 +6,12 @@ export const RemotionRoot: React.FC = () => {
   return (
     <>
       <Composition
-        id="MyComp"
+        id="TanookiZootIntro"
         component={MyComposition}
-        durationInFrames={60}
+        durationInFrames={233}
         fps={30}
-        width={1280}
-        height={720}
+        width={1920}
+        height={1080}
       />
     </>
   );
